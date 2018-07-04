6bc57918343291d8e4b3b1d21e224ee1c0e955f8
Ju111 <SchneiderJu@gmx.net>
2018-07-04 11:20:49 +0200

    rotate a word

---
+++ rotatestring.js
@@ -0,0 +1,10 @@
+function rotate (string) {
+  array = string.split('')
+  for (var i = 0; i < array.length; i++) {
+    var pop = array.pop([array.length-1])
+    array.unshift(pop).toString()
+    console.log(array.join(''))
+  }
+}
+
+rotate('sunshine')
