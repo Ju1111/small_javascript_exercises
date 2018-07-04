1ba3e9a3a8b3abab4931e26a2df553955e0d7525
Ju111 <SchneiderJu@gmx.net>
2018-07-04 14:15:18 +0200

    refactor to ES6

--- rotatestring.js
+++ rotatestring.js
@@ -1,5 +1,6 @@
-function rotate (string) {
+const rotate = string => {
   array = string.split('')
+  
   for (var i = 0; i < array.length; i++) {
     var pop = array.pop([array.length-1])
     array.unshift(pop).toString()
