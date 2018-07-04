da56f4316d0cab886ba166c79b14515eb4b2cd28
Ju111 <SchneiderJu@gmx.net>
2018-07-04 14:19:17 +0200

    fix bug in remainder of year divided by 400

--- leapyear.js
+++ leapyear.js
@@ -1,15 +1,15 @@
-function output(year) {
+const output = year => {
   return leapYear(year) ? `${year} is a leap year.` : `${year} isn't a leap year.`
 }
 
-function leapYear (year) {
+const leapYear = year => {
   if (year % 4 == 0) {
     if (year % 100 == 0) {
-      year % 400 ? true : false
+      return (year % 400 == 0) ? true : false
     }
     else
       return true
   }
 }
 
-console.log(output(2018));
+console.log(output(2000));
