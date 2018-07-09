27d1edd6effac9c8677e2734341e2f22a597623f
Ju111 <SchneiderJu@gmx.net>
2018-07-09 10:13:38 +0200

    refactoring

--- duplicates.js
+++ duplicates.js
@@ -2,11 +2,11 @@ const a = ['a', 42, 'b', 42, 'a', 42, 11, 22, 11, 3, 42, 42, 42]
 const b = [1, 2, 3, 2, 1]
 const c = [1, 2, 3]
 
-const dubbelen = i => {
+const dubbelen = array => {
 	const uitvoor = []
 
-  i.filter((value, index) => {
-  	if (index !== i.indexOf(value) && !uitvoor.includes(value)) {
+  array.filter((value, index) => {
+  	if (index !== array.indexOf(value) && !uitvoor.includes(value)) {
     	uitvoor.push(value)
     }
   })
